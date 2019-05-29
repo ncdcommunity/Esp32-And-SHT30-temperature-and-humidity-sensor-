923626d22fa3a8c86b259741b99038d837b13157
mjScientech <50486291+mjScientech@users.noreply.github.com>
2019-05-29 15:43:00 +0530

    Add files via upload

---
+++ ESP32_SHT30.ino
@@ -0,0 +1,147 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <Wire.h>
+#define WIFISSID "Dcube_web" // Put your WifiSSID here
+#define PASSWORD "D@123456" // Put your wifi password here
+#define TOKEN "BBFF-8ofHUIygUu9HwwzSZ6wrhNXEwTlgQy" // Put your Ubidots' TOKEN
+#define MQTT_CLIENT_NAME "9cdef787" // MQTT client Name, please enter your own 8-12 alphanumeric character ASCII string; 
+                                           //it should be a random and unique ascii string and different from all other devices
+
+/****************************************
+ * Define Constants
+ ****************************************/
+#define VARIABLE_LABEL "sensor" // Assing the variable label
+#define DEVICE_LABEL "esp32" // Assig the device label
+
+#define si7021Addr 0x44// Set the GPIO12 as SENSOR
+
+char mqttBroker[]  = "industrial.api.ubidots.com";
+char payload[100];
+char topic[150];
+// Space to store values to send
+char str_sensor[10];
+
+/****************************************
+ * Auxiliar Functions
+ ****************************************/
+WiFiClient ubidots;
+PubSubClient client(ubidots);
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  char p[length + 1];
+  memcpy(p, payload, length);
+  p[length] = NULL;
+  String message(p);
+  Serial.write(payload, length);
+  Serial.println(topic);
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.println("Attempting MQTT connection...");
+    
+    // Attemp to connect
+    if (client.connect(MQTT_CLIENT_NAME, TOKEN, "")) {
+      Serial.println("Connected");
+    } else {
+      Serial.print("Failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 2 seconds");
+      // Wait 2 seconds before retrying
+      delay(2000);
+    }
+  }
+}
+
+/****************************************
+ * Main Functions
+ ****************************************/
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(WIFISSID, PASSWORD);
+  // Assign the pin as INPUT 
+  Wire.begin();
+  Serial.begin(9600);
+  Wire.beginTransmission(si7021Addr);
+  Wire.endTransmission();
+  delay(300); 
+
+  Serial.println();
+  Serial.print("Wait for WiFi...");
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi Connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  client.setServer(mqttBroker, 1883);
+  client.setCallback(callback);
+   
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+
+  sprintf(topic, "%s%s", "/v1.6/devices/", DEVICE_LABEL);
+  sprintf(payload, "%s", ""); // Cleans the payload
+  sprintf(payload, "{\"%s\":", VARIABLE_LABEL); // Adds the variable label
+  
+  unsigned int data[6];
+  
+  // Start I2C Transmission
+  Wire.beginTransmission( si7021Addr);
+  // Send measurement command
+  Wire.write(0x2C);
+  Wire.write(0x06);
+  // Stop I2C transmission
+  Wire.endTransmission();
+  delay(500);
+
+  // Request 6 bytes of data
+  Wire.requestFrom( si7021Addr, 6);
+
+  // Read 6 bytes of data
+  // cTemp msb, cTemp lsb, cTemp crc, humidity msb, humidity lsb, humidity crc
+  if (Wire.available() == 6)
+  {
+    data[0] = Wire.read();
+    data[1] = Wire.read();
+    data[2] = Wire.read();
+    data[3] = Wire.read();
+    data[4] = Wire.read();
+    data[5] = Wire.read();
+  }
+
+  // Convert the data
+  float cTemp = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45;
+  float fTemp = (cTemp * 1.8) + 32;
+  float humidity = ((((data[3] * 256.0) + data[4]) * 100) / 65535.0);
+
+  // Output data to serial monitor
+  Serial.print("Relative Humidity : ");
+  Serial.print(humidity);
+  Serial.println(" %RH");
+  Serial.print("Temperature in Celsius : ");
+  Serial.print(cTemp);
+  Serial.println(" C");
+  Serial.print("Temperature in Fahrenheit : ");
+  Serial.print(fTemp);
+  Serial.println(" F");
+  delay(500);
+  
+  /* 4 is mininum width, 2 is precision; float value is copied onto str_sensor*/
+  dtostrf(cTemp, 4, 2, str_sensor);
+  
+  sprintf(payload, "%s {\"value\": %s}}", payload, str_sensor); // Adds the value
+  Serial.println("Publishing data to Ubidots Cloud");
+  client.publish(topic, payload);
+  client.loop();
+  delay(1000);
+}
